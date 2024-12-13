6d86ce742bc2cee89f5832dd995147cdc11faa4d
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-13 08:31:01 +0100

    implement intro section and nav section

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,50 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1>Stephen Oluyomi</h1>
+      <Intro />
+
+      <Nav />
     </>
   );
 }
+
+const Intro = () => {
+  return (
+    <>
+      <div>
+        Hi my name is
+        <p>Stephen Oluyomi</p>
+        <p>
+          I'm a software engineer, a mathematician fascinated by AI and Robotics
+        </p>
+      </div>
+    </>
+  );
+};
+
+const Nav: React.FC = () => {
+  return (
+    <>
+      <div>
+        <a href="#">
+          <img src="home-icon.png" alt="Home Icon" />
+          <span>Home</span>
+        </a>
+        <a href="#">
+          <img src="document-icon.png" alt="Document Icon" />
+          <span>Resume</span>
+        </a>
+        <a href="#">
+          <img src="folder-icon.png" alt="Folder Icon" />
+          <span>Project</span>
+        </a>
+        <a href="#">
+          <img src="contact-icon.png" alt="Contact Icon" />
+          <span>Contact</span>
+        </a>
+      </div>
+    </>
+  );
+};
 
 export default App;
