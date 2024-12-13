b6bb900edd1d81915b31e8bcad166728ffed902f
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-13 15:54:51 +0100

    set home, resume, project and contact pages

---
+++ src/pages/home.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Nav from "../components/nav";
+
+export const Home: React.FC = () => {
+  return (
+    <>
+      <Intro />
+      <Nav />
+    </>
+  );
+};
+
+const Intro: React.FC = () => {
+  return (
+    <div className="p-4">
+      {" "}
+      <h1 className="text-4xl">Hi,👋🏻 my name is Stephen Oluyomi</h1>{" "}
+      <p className="mt-2 text-[13px] leading-8">
+        {" "}
+        I am a <span className="font-bold">software engineer</span> and{" "}
+        <span className="font-bold">mathematician</span> with a deep fascination
+        for <span className="font-bold">AI and robotics</span>. I thrive on{" "}
+        <span className="font-bold">solving complex problems</span> and love
+        building{" "}
+        <span className="font-bold">solution-driven tech products</span> and
+        <span className="font-bold">enterprise applications</span>. I am
+        committed to{" "}
+        <span className="font-bold">becoming a better software engineer</span>{" "}
+        every day by adhering to{" "}
+        <span className="font-bold">industry-standard practices</span> and
+        focusing on building{" "}
+        <span className="font-bold">safe and secure software </span>
+      </p>{" "}
+    </div>
+  );
+};
+
+export default Home;
