d19fe23902df5599dd4030d2b6e46c9b89291ad8
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-13 08:29:46 +0100

    implement contact form

---
+++ src/components/contact.tsx
@@ -0,0 +1,40 @@
+const Contact = () => {
+  return (
+    <>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Contact</h1>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Message</span>
+                </label>
+
+                <textarea className="input input-bordered"></textarea>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">send</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contact;
