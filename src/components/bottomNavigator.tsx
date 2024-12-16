2c4832a578495ad7ec437607fa48f993150b4a47
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-16 17:04:17 +0100

    implement a simple bottom navigator

---
+++ src/components/bottomNavigator.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+interface IBottomNavigatorButton {
+  title?: string;
+}
+
+const BottomNavigatorButton: React.FC<IBottomNavigatorButton> = ({
+  title = "Resume",
+}) => {
+  return (
+    <>
+      <button className="btn flex items-center justify-center">
+        <span>{title}</span>
+        <span className="font-bold text-3xl">&#8594;</span>
+
+        {/* <img
+          width="50"
+          height="50"
+          src="https://img.icons8.com/ios/50/right--v1.png"
+          alt="right--v1"
+        />{" "} */}
+      </button>
+    </>
+  );
+};
+
+export default BottomNavigatorButton;
