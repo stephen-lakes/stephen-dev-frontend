f1566c3db8d4a6253d414472868de2445e55c120
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-16 15:52:45 +0100

    render  on Contact pages

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import ContactForm from "../components/contact";
 
 const Contact: React.FC = () => {
-  return <div>Contact</div>;
+  return <ContactForm />;
 };
 
 export default Contact;
