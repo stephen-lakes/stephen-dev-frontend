0bc11c99a630a6f1f021801decb7e2c09457b203
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-16 16:59:59 +0100

    chore: maintain single column on large screen

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -2,7 +2,7 @@ const ContactForm = () => {
   return (
     <>
       <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="hero-content flex-col">
           <div className="text-center lg:text-left">
             <h1 className="text-5xl font-bold">Contact</h1>
           </div>
