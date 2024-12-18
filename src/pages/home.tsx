51917fbefdcdc32b36acbad564f51a4868a8c8e2
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-18 11:21:08 +0100

    chore: clean up

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -24,24 +24,22 @@ export const Home: React.FC = () => {
 const Intro: React.FC = () => {
   return (
     <div className="p-4">
-      {" "}
-      <h1 className="text-4xl">Hi,👋🏻 my name is Stephen Oluyomi</h1>{" "}
+      <h1 className="text-4xl">Hi,👋🏻 my name is Stephen Oluyomi</h1>
       <p className="mt-2 text-[13px] leading-8">
-        {" "}
-        I am a <span className="font-bold">software engineer</span> and{" "}
+        I am a <span className="font-bold">software engineer</span> and
         <span className="font-bold">mathematician</span> with a deep fascination
-        for <span className="font-bold">AI and robotics</span>. I thrive on{" "}
+        for <span className="font-bold">AI and robotics</span>. I thrive on
         <span className="font-bold">solving complex problems</span> and love
-        building{" "}
+        building
         <span className="font-bold">solution-driven tech products</span> and
         <span className="font-bold">enterprise applications</span>. I am
-        committed to{" "}
-        <span className="font-bold">becoming a better software engineer</span>{" "}
-        every day by adhering to{" "}
+        committed to
+        <span className="font-bold">becoming a better software engineer</span>
+        every day by adhering to
         <span className="font-bold">industry-standard practices</span> and
-        focusing on building{" "}
+        focusing on building
         <span className="font-bold">safe and secure software </span>
-      </p>{" "}
+      </p>
     </div>
   );
 };
