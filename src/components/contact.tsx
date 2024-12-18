5d638fb5691de3b89840b639672f70445429110a
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-18 08:41:14 +0100

    add funame input field
    remove contact h1 heading

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,36 +1,42 @@
 const ContactForm = () => {
   return (
     <>
-      <div className="hero min-h-screen">
-        <div className="hero-content flex-col">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Contact</h1>
-          </div>
-          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <form className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  placeholder="email"
-                  className="input input-bordered"
-                  required
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Message</span>
-                </label>
+      <div className="hero min-h-screen px-3">
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Fullname</span>
+              </label>
+              <input
+                type="test"
+                placeholder="e.g John Doe"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                placeholder="e.g John@email.com"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Message</span>
+              </label>
 
-                <textarea className="input input-bordered"></textarea>
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary">send</button>
-              </div>
-            </form>
-          </div>
+              <textarea className="input input-bordered"></textarea>
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn btn-primary">send</button>
+            </div>
+          </form>
         </div>
       </div>
     </>
