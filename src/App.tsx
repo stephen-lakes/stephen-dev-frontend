b5d4bb0c9ef18f61f520cf7c412f23a4652900c6
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-20 19:41:30 +0100

    use react router for navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { BrowserRouter, Route, Routes } from "react-router";
 import "./App.css";
-import Index from "./pages";
+import Layout from "./pages/layout";
+import Home from "./pages/home";
+import Resume from "./pages/resume";
+import Contact from "./pages/contact";
+import NotFound from "./pages/notFound";
 
 function App() {
   return (
     <>
-      <Index />
+      <BrowserRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </Layout>
+      </BrowserRouter>
     </>
   );
 }
