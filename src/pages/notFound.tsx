6af80bddf699e8857202c617dbbd8599219c4774
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-20 19:05:00 +0100

    implement NotFound page

--- src/pages/notFound.tsx
+++ src/pages/notFound.tsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const NotFound: React.FC = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="h-svh w-svw flex flex-col items-center justify-center gap-4">
+      <h1 className="">Something went wrong😞</h1>
+      <img
+        className="w-20 h-20"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhtZh1ps1Ma66Ok1sGAAg_1GkC63OojT_ezA&s"
+        alt=""
+      />
+      <a href="">&#8592; Back to home page</a>
+    </div>
+  );
 };
 
 export default NotFound;
