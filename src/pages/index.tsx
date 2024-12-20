24b77f93b27ba404cb55feaa17c7ff9cf7d29552
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-20 23:23:55 +0100

    add projects page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import Home from "./home";
 import Resume from "./resume";
 import Contact from "./contact";
 import NotFound from "./notFound";
+import Project from "./project";
 
 const Index: React.FC = () => {
   return (
@@ -14,6 +15,7 @@ const Index: React.FC = () => {
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/home" element={<Home />} />
+            <Route path="/projects" element={<Project />} />
             <Route path="/resume" element={<Resume />} />
             <Route path="/contact" element={<Contact />} />
             <Route path="*" element={<NotFound />} />
