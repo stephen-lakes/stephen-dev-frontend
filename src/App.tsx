f44ffc7ce5fd5739c0d20d846b8606ab1a2c2a46
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-20 19:49:38 +0100

    chore: clean up
    imported index page

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router";
-import "./App.css";
-import Layout from "./pages/layout";
-import Home from "./pages/home";
-import Resume from "./pages/resume";
-import Contact from "./pages/contact";
-import NotFound from "./pages/notFound";
+import Index from "./pages";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Layout>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/resume" element={<Resume />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </Layout>
-      </BrowserRouter>
+      <Index />
     </>
   );
 }
