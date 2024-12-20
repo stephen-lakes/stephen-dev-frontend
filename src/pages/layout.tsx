ac61953c8e9671c50343f420496436d10960b86e
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-20 19:40:21 +0100

    setup layout to accept childern props

---
+++ src/pages/layout.tsx
@@ -0,0 +1,23 @@
+import BackToTopButton from "../components/backToTopButton";
+import MobileNavbar from "../components/mobileNavbar";
+import PageMeta from "../components/pageMeta";
+
+type LayoutProps = { children: ReactNode };
+
+const Layout = ({ children }: LayoutProps) => {
+  return (
+    <>
+      <div className="font-sans p-3">
+        <PageMeta title="Stephen Oluyomi - Software Engineer" favicon="" />
+        <MobileNavbar />
+        <main>
+          {/* Other pages go here */}
+          {children}
+        </main>
+        <BackToTopButton />
+      </div>
+    </>
+  );
+};
+
+export default Layout;
