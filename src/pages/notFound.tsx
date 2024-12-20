b7691277d1e9453840e992c56c8acfe2b4a7f7c4
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-20 19:07:09 +0100

    add link back to homepage

--- src/pages/notFound.tsx
+++ src/pages/notFound.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router";
 
 const NotFound: React.FC = () => {
   return (
@@ -9,7 +10,7 @@ const NotFound: React.FC = () => {
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhtZh1ps1Ma66Ok1sGAAg_1GkC63OojT_ezA&s"
         alt=""
       />
-      <a href="">&#8592; Back to home page</a>
+      <Link to="/home">&#8592; Back to home page</Link>
     </div>
   );
 };
