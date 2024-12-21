0c2f174fb90cee8636ba40671c4eb774dd8072b5
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-21 12:00:55 +0100

    chore: clean up

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -34,17 +34,17 @@ const ContactForm = () => {
 
             <textarea className="input input-bordered text-black"></textarea>
           </div>
-          <div className="mt-6">
+          <div className="mt-6 form-control">
             <button className="btn btn-primary">send</button>
           </div>
         </form>
-      </div>
-      <div className="mt-4 text-center">
-        <Link to="/">
-          <button className="rounded border-gray-950 bg-gray-950 border text-white-500 px-4 py-2  hover:text-white">
-            Home &#8594;
-          </button>
-        </Link>
+        <div className="mt-4 text-center lg:hidden">
+          <Link to="/">
+            <button className="rounded border-gray-950 bg-gray-950 border text-white-500 px-4 py-2  hover:text-white">
+              Home &#8594;
+            </button>
+          </Link>
+        </div>
       </div>
     </>
   );
