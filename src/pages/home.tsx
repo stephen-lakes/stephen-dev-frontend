16233a7943db138ebeb62a3b999e5c9c08112070
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-21 12:01:54 +0100

    implement a new home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,80 +1,11 @@
 import React from "react";
-import PageMeta from "../components/pageMeta";
-import homeFavicon from "../assets/github.svg";
-import Resume from "./resume";
-import myPic from "../assets/me.jpg";
-import { Link } from "react-router";
+import Profile from "../components/profile";
 
 export const Home: React.FC = () => {
   return (
     <>
-      <PageMeta title="Stephen Oluyomi" favicon={homeFavicon} />
-      <div className="max-h-screen md:flex lg:px-10">
-        <div className="lg:w-2/5 lg:block lg:mt-24">
-          <Profile />
-        </div>
-        <div className="hidden lg:block lg:w-3/5 lg:overflow-y-scroll lg:no-scrollbar shadow-2xl">
-          <Resume />
-        </div>
-      </div>
+      <Profile />
     </>
-  );
-};
-// class="flex flex-col items-center text-center"
-const Bio = () => {
-  return (
-    <>
-      <h1 className="mt-2 text-2xl font-[600]">Hi,👋🏻 I'm Stephen Oluyomi</h1>
-      <p className="mt-2 mb-6 text-[13px] leading-8">
-        {/* I am a <span className="font-bold">software engineer</span>,
-        <span className="font-bold"> mathematician</span> with a deep
-        fascination for <span className="font-bold">AI and robotics</span>. I
-        thrive on
-        <span className="font-bold"> solving complex problems</span> and love
-        building solution-driven tech products and
-        <span className="font-bold"> enterprise applications</span>. I am
-        committed to
-        <span className="font-bold"> becoming a better software engineer </span>
-        every day by adhering to industry-standard practices and focusing on
-        building
-        <span className="font-bold"> safe </span> and
-        <span className="font-bold"> secure</span> software */}
-        I’m a software engineer and mathematician with a passion for AI and
-        robotics. I excel at solving complex problems and building tech
-        solutions. Becoming a better engineer one day at a time, I'm big on
-        security 🔒
-      </p>
-
-      <div className="mt-4 text-center lg:hidden">
-        <Link to="/projects">
-          <button className="rounded border-gray-950 bg-gray-950 border text-white-500 px-4 py-2  hover:text-white">
-            Explore &#8594;
-          </button>
-        </Link>
-      </div>
-    </>
-  );
-};
-const ProfilePicture = () => {
-  return (
-    <>
-      <div className="w-[192px] h-[192px] overflow-hidden rounded-full lg:hidden">
-        <img
-          src={myPic}
-          alt=""
-          className="translate-y-[-70px] transfrom scale-150"
-        />
-      </div>
-    </>
-  );
-};
-
-export const Profile = () => {
-  return (
-    <div className="flex flex-col items-center text-center">
-      <ProfilePicture />
-      <Bio />
-    </div>
   );
 };
 
