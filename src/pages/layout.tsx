21074c42dd509fea3362044be7b639a40bfdf637
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-21 11:58:07 +0100

    restructure layout page
    create app layout

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,29 +1,35 @@
 import { ReactNode } from "react";
-import BackToTopButton from "../components/backToTopButton";
 import MobileNavbar from "../components/mobileNavbar";
-import PageMeta from "../components/pageMeta";
 import MobileFooter from "../components/mobileFooter";
+import Nav from "../components/nav";
+import SocialCard from "../components/socialCard";
+import Profile from "../components/profile";
 
 type LayoutProps = { children: ReactNode };
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <>
-      <div className="font-sans px-3">
-        <PageMeta title="Stephen Oluyomi - Software Engineer" favicon="" />
-        <header className="lg:hidden">
-          <MobileNavbar />
-        </header>
-        <main className="">
-          {/* Other pages go here */}
+    <div className="flex flex-col min-h-screen">
+      <header className="lg:hidden p-4">
+        <MobileNavbar />
+      </header>
+
+      <div className="text-white flex flex-grow lg:h-screen lg:overflow-y-hidden">
+        <aside className="hidden lg:w-1/2 p-4 lg:flex lg:flex-col lg:justify-evenly">
+          <Profile />
+          <Nav />
+          <SocialCard />
+        </aside>
+
+        <main className="w-full lg:w-1/2 p-4 flex items-center justify-center lg:overflow-y-scroll">
           {children}
         </main>
-        <BackToTopButton />
-        <footer className="lg:hidden">
-          <MobileFooter />
-        </footer>
       </div>
-    </>
+
+      <footer className="lg:hidden p-4 mt-auto">
+        <MobileFooter />
+      </footer>
+    </div>
   );
 };
 
