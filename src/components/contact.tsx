4a82e9d48572aef91e345346e6ce001979d15ea2
stephen-lakes <oluyomiolamilekan99@gmail.com>
2024-12-21 01:26:48 +0100

    change input text color to white
    add button to navigate to the next page

--- src/components/contact.tsx
+++ src/components/contact.tsx
@@ -1,43 +1,50 @@
+import { Link } from "react-router";
+
 const ContactForm = () => {
   return (
     <>
-      <div className="hero min-h-screen px-3">
-        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-          <form className="card-body">
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Fullname</span>
-              </label>
-              <input
-                type="test"
-                placeholder="e.g John Doe"
-                className="input input-bordered"
-                required
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                placeholder="e.g John@email.com"
-                className="input input-bordered"
-                required
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Message</span>
-              </label>
+      <div className="card w-full max-w-sm shrink-0 shadow-2xl mt-10">
+        <form className="">
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text text-white">Fullname</span>
+            </label>
+            <input
+              type="text"
+              placeholder="e.g John Doe"
+              className="input input-bordered text-black"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text text-white">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="e.g John@email.com"
+              className="input input-bordered text-black"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text text-white">Message</span>
+            </label>
 
-              <textarea className="input input-bordered"></textarea>
-            </div>
-            <div className="form-control mt-6">
-              <button className="btn btn-primary">send</button>
-            </div>
-          </form>
-        </div>
+            <textarea className="input input-bordered text-black"></textarea>
+          </div>
+          <div className="mt-6">
+            <button className="btn btn-primary">send</button>
+          </div>
+        </form>
+      </div>
+      <div className="mt-4 text-center">
+        <Link to="/">
+          <button className="rounded border-gray-950 bg-gray-950 border text-white-500 px-4 py-2  hover:text-white">
+            Home &#8594;
+          </button>
+        </Link>
       </div>
     </>
   );
